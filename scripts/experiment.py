ac254fe5f2644ca72d2169252bd04182d81badf0
Tomas H <horychtomas98@gmail.com>
2023-11-09 18:17:51 +0900

    updated the training script

--- scripts/experiment.py
+++ scripts/experiment.py
@@ -11,20 +11,19 @@ from torch.utils.data import DataLoader
 from similearn import SentenceTransformer
 import logging
 import pickle
-from peft import get_peft_model, LoraConfig
 logger = logging.getLogger(__name__)
 
 
 # Load the dataset
-model_name = "thenlper/gte-base"
+model_name = "roberta-base"
 run_name = model_name.split("/")[-1] + "-fine-tuning"
 wandbc = WandbClient(run_name=run_name)
 
 model = SentenceTransformer(model_name, wandbc=wandbc)
 
 # load datasets from wandb
-train_dataset = pd.read_csv(wandbc.load_dataset("zbmath_train"))
-dev_dataset = pd.read_csv(wandbc.load_dataset("zbmath_dev"))
+train_dataset = pd.read_parquet(wandbc.load_dataset("zbmath_open_train"))
+dev_dataset = pd.read_parquet(wandbc.load_dataset("zbmath_open_dev"))
 
 train_examples = []
 dev_examples = []
@@ -36,18 +35,11 @@ for i, row in tqdm(train_dataset.iterrows(), total=len(train_dataset)):
 for i, row in tqdm(dev_dataset.iterrows(), total=len(dev_dataset)):
     dev_examples.append(InputExample(texts=[row["text_x"], row["text_y"]]))
 
-train_dataloader = DataLoader(train_examples, shuffle=True, batch_size=32)
-dev_dataloader = DataLoader(dev_examples, shuffle=True, batch_size=32)
+train_dataloader = DataLoader(train_examples, shuffle=True, batch_size=16)
+dev_dataloader = DataLoader(dev_examples, shuffle=True, batch_size=16)
 
 train_loss = MultipleNegativesRankingLoss(model=model)
 
-peft_config = LoraConfig(
-    lora_alpha=16,
-    lora_dropout=0.1,
-    r=4,
-    target_modules=["query","key","value"]
-)
-train_loss = get_peft_model(train_loss,peft_config=peft_config)
 
 with open(wandbc.load_dataset("queries"), 'rb') as file:
     queries = pickle.load(file)
@@ -62,14 +54,28 @@ with open(wandbc.load_dataset("relevant_docs"), 'rb') as file:
 evaluator = InformationRetrievalEvaluator(queries=queries,corpus=corpus,relevant_docs=relevant_docs,wandbc=wandbc)
 
 training_args = {
-    "epochs": 1,
+    "epochs": 2,
     "scheduler": "WarmupLinear",
     "warmup_steps": 100,
     "evaluator": evaluator,
-    "evaluation_steps": 100,
-    "checkpoint_save_steps": 500,
+    "evaluation_steps": 200,
+    "checkpoint_save_steps": 1000,
     "checkpoint_save_total_limit": 3,
 }
 
+logger.info("Starting training...")
 model.fit(train_objectives=[(train_dataloader, train_loss)], **training_args)
+
+
+logging.info("Uploading model...")
+base_model = train_loss.model[0].auto_model
+tokenizer = train_loss.model.tokenizer
+
+base_model.push_to_hub(
+    "horychtom/" + model_name.split("/")[-1] + "-zbmath-open",
+)
+tokenizer.push_to_hub(
+    "horychtom/" + model_name.split("/")[-1] + "-zbmath-open",
+)
+
 wandbc.finish()
